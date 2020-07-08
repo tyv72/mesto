463e8c64a1139c03aeacc2a8f3ca5df6ca0ed2ee
Yuliya Tsareva <tyv72@yandex.ru>
2020-07-08 23:30:46 +0700

    feat: добавить основную страницу и стили, добавить форму и скрипт для работы с ней

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+let closeButton = document.querySelector('.close-button');
+let editButton = document.querySelector('.profile-info__edit-button');
+
+let popup = document.querySelector('.popup');
+let popupNameField = document.querySelector('.popup__field_item_name');
+let popupAboutField = document.querySelector('.popup__field_item_about');
+let popupForm = document.querySelector('.popup__container');
+
+let nameField = document.querySelector('.profile-info__name');
+let aboutField = document.querySelector('.profile-info__about');
+
+let closePopup = function() {
+  popup.classList.remove('popup_opened');
+  popup.classList.add('popup_closed');
+}
+
+let openPopup = function() {
+  popup.classList.add('popup_opened');
+  popup.classList.remove('popup_closed'); 
+
+  popupNameField.value = nameField.innerText;
+  popupAboutField.value = aboutField.innerText;
+}
+
+let saveInfo = function(event) {
+  event.preventDefault();
+
+  nameField.textContent = popupNameField.value;  
+  aboutField.textContent = popupAboutField.value;
+
+  closePopup();  
+}
+
+closeButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', openPopup);
+popupForm.addEventListener('submit', saveInfo);
